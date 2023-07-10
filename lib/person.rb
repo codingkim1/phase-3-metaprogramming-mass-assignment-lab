aa924f793021b490ef39843205a4c94857da86de
Alex <alexkimanthi92@gmail.com>
2023-07-10 14:46:16 +0300

    updated

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+alex_attributes = { name: "Alex", handed: "right"}
+p alex = Person.new(alex_attributes)
+
+
